693673e4ad54e26031d21f4ee2392f8deed19040
Tetat <tetat.chulchue@gmail.com>
2019-08-07 17:58:37 +0700

    revert admin  credential

--- functions/src/config/firebase.js
+++ functions/src/config/firebase.js
@@ -1,7 +1,9 @@
 const admin = require('firebase-admin');
 
+const adminFirebase = process.env.FIREBASE_CONFIG;
+
 admin.initializeApp({
-    credential: admin.credential.cert(functions.config().admin),
+    credential: admin.credential.cert(adminFirebase),
     databaseURL: "https://itfreshy2019.firebaseio.com"
 });
 
