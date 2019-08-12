eb140ec50414a0b4fd08ddb9fade91275417a4c4
Icyscools <meet.ice1@gmail.com>
2019-08-12 23:47:26 +0700

    Initialize Vuex

--- src/store.js
+++ src/store.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import axios from "axios"
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    user: ""
+  },
+  getters: {
+    getUser: (state) => {
+      return this.user
+    }
   },
   mutations: {
-
+    setUser: (state, user) => {
+      state.user = user
+    } 
   },
   actions: {
 
-  }
+  },
+  plugins: [createPersistedState({
+    key: 'ophkmitl',
+    storage: {
+      getItem: key => Cookies.get(key),
+      setItem: (key, value) => Cookies.set(key, value, { expires: 1, secure: false, }),
+      removeItem: key => Cookies.remove(key)
+    },
+  })],
 })
